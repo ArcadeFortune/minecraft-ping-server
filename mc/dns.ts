b6dd0a766b5ad7ca9772d69d545b172d9b458c0b
lama <praktikant@mail.cyc.lan>
2026-09-22 17:48:07 +0200

    fix: can truly handle all server addresses starting with http(s)://

--- mc/dns.ts
+++ mc/dns.ts
@@ -2,12 +2,13 @@ import { Util } from "@/mc/util.ts";
 
 export class DNS {
   static async resolve(name: string) {
+    name = name.replace(/^https?:\/\//i, "");
     const match = name.match(/^(?<hostname>[^:]+)(?::(?<port>\d+))?$/);
     if (match?.groups?.port) {
       return { target: match.groups.hostname, port: Number(match.groups.port) };
     }
     try {
-      const ips = await Deno.resolveDns("_minecraft._tcp." + name.replace(/^https?:\/\//i, ''), "SRV");
+      const ips = await Deno.resolveDns("_minecraft._tcp." + name, "SRV");
       return { target: ips[0].target, port: ips[0].port };
     } catch (_) {
       return { target: name, port: Util.DEFAULT_SERVER_PORT };
